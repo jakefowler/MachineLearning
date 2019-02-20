1eccbe7f7319741eadc67046203501970ae2711f
jakefowler <jake@Jakes-MacBook-Pro.local>
2019-02-19 18:55:04 -0700

    averaged the error and worked on updating weights

--- Perceptron/perceptronClassifier.py
+++ Perceptron/perceptronClassifier.py
@@ -11,6 +11,7 @@ def trainPerceptron(csvFileName):
     Function that takes in a csv file name and trains the perceptron on the data.
     :param: csvFileName -> file name a directory if not in the same directory
     """
+    learningRate = 0.01
     data = pd.read_csv(csvFileName)
     print(data)
     print(len(data.columns))
@@ -33,10 +34,24 @@ def trainPerceptron(csvFileName):
     target = data.iloc[:, -1]
     error = output - target
     print(error)
+    avrError = np.mean(error)
+    print(avrError)
+
+    print(weights)
+    for val in weights:
+        val = val - learningRate * avrError
  #  shuffled_data = shuffle(data)
  #  print(shuffled_data)
  #  shuffled_data.reset_index().iloc[:, 1:]
  #  print(shuffled_data)
+
+ # need to bound the error. Average all the error rates and normalize
+ # number of epochs
+ # each epoch needs an accuracy score
+ # an epoch is one run through the training data and then test on that
+ # average all the accuracies of the folds
+ # weights carry over for each epic on each fold
+ # 
 
 
 def main():

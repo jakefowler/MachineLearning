fe72b0779017ddfa597c1da4815791da45ac96e2
jakefowler <35507951+jakefowlers@users.noreply.github.com>
2019-02-20 21:29:15 -0700

    Update perceptronClassifier.py

--- Perceptron/perceptronClassifier.py
+++ Perceptron/perceptronClassifier.py
@@ -1,6 +1,7 @@
 import sklearn
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 import pprint as pp
@@ -24,22 +25,55 @@ def trainPerceptron(csvFileName):
     print(weights)
     trained = np.dot(normalizedData,weights)
     print(trained)
-    output = []
-    for val in trained:
-        if val > 0:
-            output.append(1)
-        else:
-            output.append(-1)
+    output = np.where(trained>0,1,-1)
+    #output = np.array(shape=(768,1))
+    # output = []
+    #print(output)
+    #for val in trained:
+    #    if val > 0:
+    #        output.append(1)
+    #    else:
+    #        output.append(-1)
+    print("------Output------")
+    
     print(output)
-    target = data.iloc[:, -1]
+    target = data.iloc[:, -1].values
+    target = target.reshape(768,1)
+    #data = data.values
+    #target = data.values
+    print("------Target------")
+    print(target)
+
+    print("Accuracy = ", calculateAccuracy(target, output))
+
     error = output - target
+    print("------Error------")
     print(error)
     avrError = np.mean(error)
+    print("------AverageError------")
     print(avrError)
 
+    print("------Weights------")
     print(weights)
-    for val in weights:
-        val = val - learningRate * avrError
+
+    print("------NormalizedDataBefore------")
+    #print(normalizedData)
+    #normalizedData = np.transpose(normalizedData)
+    print("------NormalizedDataAfter------")
+    print(normalizedData)
+    print("------TransposedError------")
+    print(np.transpose(error))
+    print("------WeightsBefore------")
+    print(weights)
+    #print("Shape of weights", weights.shape())
+    #print("Shape of normalizedData", normalizedData.shape())
+    #print("Shape of error ", error.shape())
+    weights -= learningRate*np.dot(np.transpose(normalizedData), error)
+    print("------NewWeights------")
+    print(weights)
+
+   # for row in data:
+     #   val = val - learningRate * avrError
  #  shuffled_data = shuffle(data)
  #  print(shuffled_data)
  #  shuffled_data.reset_index().iloc[:, 1:]
@@ -52,6 +86,9 @@ def trainPerceptron(csvFileName):
  # average all the accuracies of the folds
  # weights carry over for each epic on each fold
  # 
+
+def calculateAccuracy(correct, prediction):
+    return accuracy_score(correct, prediction)
 
 
 def main():

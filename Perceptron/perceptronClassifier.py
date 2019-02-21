6bc57e45d00e43902e9bad39c422d155ea60edcf
jakefowler <35507951+jakefowlers@users.noreply.github.com>
2019-02-20 22:52:46 -0700

    Update perceptronClassifier.py

--- Perceptron/perceptronClassifier.py
+++ Perceptron/perceptronClassifier.py
@@ -1,10 +1,12 @@
 import sklearn
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 import pprint as pp
+import matplotlib.pylab as plt 
 
 
 def trainPerceptron(csvFileName):
@@ -12,72 +14,42 @@ def trainPerceptron(csvFileName):
     Function that takes in a csv file name and trains the perceptron on the data.
     :param: csvFileName -> file name a directory if not in the same directory
     """
+    epochAccuracies = []
+    totalAccuracies = []
     learningRate = 0.01
     data = pd.read_csv(csvFileName)
-    print(data)
-    print(len(data.columns))
     scaler = MinMaxScaler().fit(data.iloc[:, :-1])
-    #print(scaler.mean_)
-    print(scaler.scale_)
     normalizedData = scaler.transform(data.iloc[:, :-1])
-    print(normalizedData)
-    weights = np.array(np.random.randn(len(data.columns)-1,1))
-    print(weights)
-    trained = np.dot(normalizedData,weights)
-    print(trained)
-    output = np.where(trained>0,1,-1)
-    #output = np.array(shape=(768,1))
-    # output = []
-    #print(output)
-    #for val in trained:
-    #    if val > 0:
-    #        output.append(1)
-    #    else:
-    #        output.append(-1)
-    print("------Output------")
-    
-    print(output)
-    target = data.iloc[:, -1].values
-    target = target.reshape(768,1)
-    #data = data.values
-    #target = data.values
-    print("------Target------")
-    print(target)
+    bias = np.ones((768,1), dtype=np.float64)
+    normalizedData = np.append(normalizedData, bias, axis=1)
+    weights = np.array(np.random.randn(len(data.columns),1))
 
-    print("Accuracy = ", calculateAccuracy(target, output))
-
-    error = output - target
-    print("------Error------")
-    print(error)
-    avrError = np.mean(error)
-    print("------AverageError------")
-    print(avrError)
-
-    print("------Weights------")
-    print(weights)
-
-    print("------NormalizedDataBefore------")
-    #print(normalizedData)
-    #normalizedData = np.transpose(normalizedData)
-    print("------NormalizedDataAfter------")
-    print(normalizedData)
-    print("------TransposedError------")
-    print(np.transpose(error))
     print("------WeightsBefore------")
     print(weights)
-    #print("Shape of weights", weights.shape())
-    #print("Shape of normalizedData", normalizedData.shape())
-    #print("Shape of error ", error.shape())
-    weights -= learningRate*np.dot(np.transpose(normalizedData), error)
-    print("------NewWeights------")
-    print(weights)
 
-   # for row in data:
-     #   val = val - learningRate * avrError
- #  shuffled_data = shuffle(data)
- #  print(shuffled_data)
- #  shuffled_data.reset_index().iloc[:, 1:]
- #  print(shuffled_data)
+    target = data.iloc[:, -1].values
+    target = target.reshape(768,1)
+
+    skf = StratifiedKFold(n_splits=3, shuffle=True)
+    for train, test in skf.split(normalizedData, target):
+        for i in range(1000):
+            trained = np.dot(normalizedData,weights)
+            output = np.where(trained>0,1,-1)
+            epochAccuracies.append(calculateAccuracy(target, output))
+            error = output - target
+            weights -= learningRate*np.dot(np.transpose(normalizedData), error)
+            i += 1
+        totalAccuracies.append(np.mean(epochAccuracies))
+        print("------NewWeights------")
+        print(weights)
+        print("The average accuracy over the fold is", np.mean(epochAccuracies))
+
+        plt.plot(epochAccuracies, "bo")
+        plt.show()
+        weights = np.array(np.random.randn(len(data.columns),1))
+        epochAccuracies = []
+    print("Average accuracy for this perceptron is", np.mean(totalAccuracies))
+    print("The learning rate was set to", learningRate)
 
  # need to bound the error. Average all the error rates and normalize
  # number of epochs
